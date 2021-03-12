7e8656673dea37a1d960014f4b0805c9e71e303e
Joey Li <joey@clarifyhealth.com>
2021-03-12 16:12:15 +0800

    lint error

--- projects/ng-table-virtual-scroll/src/lib/fixed-size-table-virtual-scroll-strategy.ts
+++ projects/ng-table-virtual-scroll/src/lib/fixed-size-table-virtual-scroll-strategy.ts
@@ -41,7 +41,7 @@ export class FixedSizeTableVirtualScrollStrategy implements VirtualScrollStrateg
 
   public attach(viewport: CdkVirtualScrollViewport): void {
     this.viewport = viewport;
-    // did know why, there are two observers. And that will cause 
+    // did know why, there are two observers. And that will cause
     // TableItemSizeDirective::connectDataSource receive two notification for one date changes.
     // for big and complex mat-table, that will slow down performance.
     // check the observers here to make sure only one observer exist.
